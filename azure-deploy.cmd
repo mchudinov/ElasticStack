d951d423eb98f4168600688d1f6d1fd44f44f83f
Mikael Chudinov <mikael@chudinov.net>
2019-04-11 13:48:28 +0200

    Use create app command.

--- azure-deploy.cmd
+++ azure-deploy.cmd
@@ -1,1 +1,1 @@
-az webapp update --resource-group fmdclient --plan fmdclient --name elasticstack --multicontainer-config-type compose --multicontainer-config-file docker-compose.yml
+az webapp create --resource-group fmdclient --plan fmdclient --name elasticstack --multicontainer-config-type compose --multicontainer-config-file docker-compose.yml
