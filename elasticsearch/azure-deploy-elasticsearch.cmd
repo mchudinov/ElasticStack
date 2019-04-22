d0d5c56039f0e754ef3de3511aa14ac4256b2e14
Mikael Chudinov <mikael@chudinov.net>
2019-04-22 10:53:49 +0200

    Separate parts.

--- elasticsearch/azure-deploy-elasticsearch.cmd
+++ elasticsearch/azure-deploy-elasticsearch.cmd
@@ -1,1 +1,3 @@
-az webapp create --resource-group elastic --plan elastic --name elastic2 --deployment-container-image-name chudinov/elasticsearch
+REM az webapp create --resource-group elastic --plan elastic --name elastic2 --deployment-container-image-name chudinov/elasticsearch
+
+REM az webapp create --resource-group elasticsearch --plan elasticsearch --name elasticsearchmc --multicontainer-config-type compose --multicontainer-config-file .\docker-compose.elasticsearch.azure.yml
