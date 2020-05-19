a532da27b60a8794611922b9f6e44ae46719e32d
Mikael Chudinov <mikael@chudinov.net>
2020-05-20 01:34:51 +0200

    Disable security xpack.

--- elasticsearch/buildrun.bat
+++ elasticsearch/buildrun.bat
@@ -1,2 +1,2 @@
-docker build --build-arg IMAGE_VERSION=7.7.0 -t chudinov/elasticsearch .
+docker build --build-arg IMAGE_VERSION=7.6.2 -t chudinov/elasticsearch .
 docker run -d -it -p 9200:9200 --name elasticsearch chudinov/elasticsearch 
