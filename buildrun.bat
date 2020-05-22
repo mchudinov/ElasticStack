3ad4bf5906419a30c81b89650b8b961129e9cbae
Mikael Chudinov <mikael@chudinov.net>
2020-05-22 15:33:24 +0200

    Add build arguments to compose.

--- buildrun.bat
+++ buildrun.bat
@@ -1,2 +1,6 @@
-docker-compose build --build-arg AZURE_DEFAULT_ACCOUNT=dataverksted --build-arg AZURE_DEFAULT_KEY=f3Fx9LpUgzATh6ObarQjctf4TCDsFFfP97pUZICh/JWFMkUIb7TCYIGtahAkqhUYTMcemAcQB8yeunx3OcAhSw==
+docker-compose build `
+    --build-arg AZURE_DEFAULT_ACCOUNT=dataverksted `
+    --build-arg AZURE_DEFAULT_KEY=f3Fx9LpUgzATh6ObarQjctf4TCDsFFfP97pUZICh/JWFMkUIb7TCYIGtahAkqhUYTMcemAcQB8yeunx3OcAhSw== `
+    --build-arg AWS_ACCESS_KEY=admin `
+    --build-arg AWS_SECRET_KEY=minioadmin `
 docker-compose up -d
--- elasticsearch/buildrun.bat
+++ elasticsearch/buildrun.bat
@@ -1,2 +1,8 @@
-docker build --build-arg IMAGE_VERSION=7.6.2 --build-arg AZURE_DEFAULT_ACCOUNT=dataverksted --build-arg AZURE_DEFAULT_KEY=f3Fx9LpUgzATh6ObarQjctf4TCDsFFfP97pUZICh/JWFMkUIb7TCYIGtahAkqhUYTMcemAcQB8yeunx3OcAhSw== -t chudinov/elasticsearch .
+docker build `
+ --build-arg IMAGE_VERSION=7.6.2 `
+ --build-arg AZURE_DEFAULT_ACCOUNT=dataverksted `
+ --build-arg AZURE_DEFAULT_KEY=f3Fx9LpUgzATh6ObarQjctf4TCDsFFfP97pUZICh/JWFMkUIb7TCYIGtahAkqhUYTMcemAcQB8yeunx3OcAhSw== `
+ --build-arg AWS_ACCESS_KEY=admin `
+ --build-arg AWS_SECRET_KEY=minioadmin `
+ -t chudinov/elasticsearch .
 docker run -d -it -p 9200:9200 --name elasticsearch chudinov/elasticsearch 
