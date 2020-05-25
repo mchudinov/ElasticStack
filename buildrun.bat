ed9017f07e317f2124aa04fb131fd6a3fa941eb2
Mikael Chudinov <mikael@chudinov.net>
2020-05-25 12:37:58 +0200

    Add metricbeat.

--- buildrun.bat
+++ buildrun.bat
@@ -2,5 +2,12 @@ docker-compose build `
     --build-arg AZURE_DEFAULT_ACCOUNT=dataverksted `
     --build-arg AZURE_DEFAULT_KEY=f3Fx9LpUgzATh6ObarQjctf4TCDsFFfP97pUZICh/JWFMkUIb7TCYIGtahAkqhUYTMcemAcQB8yeunx3OcAhSw== `
     --build-arg AWS_ACCESS_KEY=admin `
-    --build-arg AWS_SECRET_KEY=minioadmin `
+    --build-arg AWS_SECRET_KEY=minioadmin
+
 docker-compose up -d
+
+docker-compose build \
+    --build-arg AZURE_DEFAULT_ACCOUNT=dataverksted \
+    --build-arg AZURE_DEFAULT_KEY=f3Fx9LpUgzATh6ObarQjctf4TCDsFFfP97pUZICh/JWFMkUIb7TCYIGtahAkqhUYTMcemAcQB8yeunx3OcAhSw== \
+    --build-arg AWS_ACCESS_KEY=admin \
+    --build-arg AWS_SECRET_KEY=minioadmin 
