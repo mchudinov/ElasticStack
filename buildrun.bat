cd72248d0895f0ed715ec44a8239e89706f57a8d
Mikael Chudinov <mikael@chudinov.net>
2020-06-09 12:07:15 +0200

    Remove key from code.

--- buildrun.bat
+++ buildrun.bat
@@ -1,7 +1,7 @@
 docker-compose build `
     --build-arg AZURE_DEFAULT_ACCOUNT=dataverksted `
-    --build-arg AZURE_DEFAULT_KEY=f3Fx9LpUgzATh6ObarQjctf4TCDsFFfP97pUZICh/JWFMkUIb7TCYIGtahAkqhUYTMcemAcQB8yeunx3OcAhSw== `
-    --build-arg AWS_ACCESS_KEY=admin `
+    --build-arg AZURE_DEFAULT_KEY=f3...== `
+    --build-arg AWS_ACCESS_KEY=minioadmin `
     --build-arg AWS_SECRET_KEY=minioadmin
 
 docker-compose up -d
@@ -9,5 +9,5 @@ docker-compose up -d
 docker-compose build \
     --build-arg AZURE_DEFAULT_ACCOUNT=xxxx \
     --build-arg AZURE_DEFAULT_KEY=f3Fxxxxxx== \
-    --build-arg AWS_ACCESS_KEY=admin \
+    --build-arg AWS_ACCESS_KEY=minioadmin \
     --build-arg AWS_SECRET_KEY=minioadmin 
