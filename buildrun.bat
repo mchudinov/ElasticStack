c2a0c9dd1ae738e26b15334b8ec9e6c60caf2ff7
Mikael Chudinov <mikael@chudinov.net>
2020-11-18 15:24:32 +0100

    Update logstash.

--- buildrun.bat
+++ buildrun.bat
@@ -3,11 +3,11 @@ REM AZURE_DEFAULT_ACCOUNT, AWS_ACCESS_KEY vaiables are for Elasticsearch backup 
 
 docker-compose build `
     --build-arg AZURE_DEFAULT_ACCOUNT=dataverksted `
-    --build-arg AZURE_DEFAULT_KEY=f3...== `
+    --build-arg AZURE_DEFAULT_KEY=f3xxx== `
     --build-arg AWS_ACCESS_KEY=minioadmin `
     --build-arg AWS_SECRET_KEY=minioadmin
 
-docker-compose build --build-arg AZURE_DEFAULT_ACCOUNT=dataverksted --build-arg AZURE_DEFAULT_KEY=f3...== --build-arg AWS_ACCESS_KEY=minioadmin --build-arg AWS_SECRET_KEY=minioadmin
+docker-compose build --build-arg AZURE_DEFAULT_ACCOUNT=dataverksted --build-arg AZURE_DEFAULT_KEY=f3xxx== --build-arg AWS_ACCESS_KEY=minioadmin --build-arg AWS_SECRET_KEY=minioadmin
 
 docker-compose up -d
 
